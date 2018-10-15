cca938bac81fada275647eff8fd0240911284768
Jens Pettersson <jens@webbhuset.se>
2018-10-15 14:43:34 +0000

    if a sveaorder is already cancelled, cancel it in magento

--- Gateway/Request/CancelOrderBuilder.php
+++ Gateway/Request/CancelOrderBuilder.php
@@ -23,19 +23,29 @@ class CancelOrderBuilder implements OrderActionBuilderInterface
      * @var Configuration
      */
     protected $apiConfig;
+    protected $messageManager;
 
     public function __construct(
         OrderHelper $helper,
-        Configuration $apiConfig
+        Configuration $apiConfig,
+        \Magento\Framework\Message\ManagerInterface $messageManager
     ) {
         $this->helper = $helper;
         $this->apiConfig = $apiConfig;
+        $this->messageManager = $messageManager;
     }
 
     public function build(\Magento\Sales\Model\Order\Payment $payment)
     {
         $order = $payment->getOrder();
         $sveaOrderId = $order->getExtOrderId();
+        $sveaOrder = $this->helper->fetchSveaOrder($order);
+
+        if ($this->isCancelled($sveaOrder)) {
+            $this->messageManager->addSuccessMessage(__('Order is already cancelled in Svea. Syncing status.'));
+
+            return false;
+        }
 
         $countryCode = $this->helper->getCountryCode($order);
 
@@ -45,5 +55,17 @@ class CancelOrderBuilder implements OrderActionBuilderInterface
             ->setCountryCode($countryCode);
 
         return $request;
+    }
+
+    protected function isCancelled($sveaOrder) {
+        if (isset($sveaOrder->orderDeliveryStatus) && $sveaOrder->orderDeliveryStatus == 'Cancelled') {
+            return true;
+        }
+
+        if (isset($sveaOrder->status) && $sveaOrder->status == 'ANNULLED') {
+            return true;
+        }
+
+        return false;
     }
 }
